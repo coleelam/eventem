88dd8fe8c9401cb814e3f14f7ca22594f3e4ca19
coleelam <elamc@purdue.edu>
2018-04-13 13:16:01 -0400

    fixed some things with the database, added new routes

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,9 @@ const { Pool } = require('pg');
 const QueryStream = require('pg-query-stream');
 const JSONStream = require('JSONStream');
 const path = require('path');
-// const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/coleelam';
-const connectionString = 'postgres://localhost:5432/coleelam';
+const moment = require('moment');
+const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/coleelam';
+// const connectionString = 'postgres://localhost:5432/coleelam';
 
 // const client = new pg.Client(connectionString);
 const pool = new Pool({
@@ -16,29 +17,57 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Eventem' });
 });
 
+/* POST NEW USER ROUTE */
 router.post('/api/users', (req, res, next) => {
   // Grab data from http request
   const data = {username: req.body.username, email: req.body.email, pass_hash: req.body.pass_hash };
   // Get a Postgres client from the connection pool
   console.log(data);
-  console.log(connectionString);
+  // console.log(connectionString);
   pool.query('INSERT INTO users (username, email, pass_hash) values($1, $2, $3)',
     [data.username, data.email, data.pass_hash], (err, res) => {
     if (err) {
-      throw err
+      console.log('Cannot add user because: ' + err);
     }
   });
+  res.render('index', { title: 'Eventem' });
 });
 
+/* GET LIST OF USERS */
 router.get('/api/users', (req, res, next) => {
-  pool.query('SELECT user_id, username, email FROM users ORDER BY id ASC', (err, res) => {
+  pool.query('SELECT user_id, username, email FROM users ORDER BY user_id ASC', (err, res) => {
     if (err) {
-      throw err
+      console.log('Cannot SELECT because: ' + err);
     }
     console.log(res.rows);
     return res.rows;
   });
+  res.render('index', { title: 'Eventem' });
 });
 
+/* POST NEW EVENT */
+router.post('/api/events', (req, res, next) => {
+  const data = {event_name: req.body.event_name, creator: req.body.creator, event_time: moment().add(3, 'days').format()}
+  console.log(data);
+  pool.query('INSERT INTO events (event_name, creator, event_time) values($1, $2, $3)',
+    [data.event_name, data.creator, data.event_time], (err, res) => {
+    if (err) {
+      console.log('Cannot add event because: ' + err);
+    }
+  });
+  res.render('index', { title: 'Eventem' });
+});
+
+/* GET LIST OF EVENTS */
+router.get('/api/events', (req, res, next) => {
+  pool.query('SELECT * FROM events ORDER BY event_time ASC', (err, res) => {
+    if (err) {
+      console.log('Cannot SELECT because: ' + err);
+    }
+    console.log(res.rows);
+    return res.rows;
+  });
+  res.render('index', { title: 'Eventem' });
+});
 
 module.exports = router;
