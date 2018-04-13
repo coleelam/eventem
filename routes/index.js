a574dd262193483fe01a2dc4f13afb6e990fa815
coleelam <elamc@purdue.edu>
2018-04-12 21:07:58 -0400

    get some of database more set up

--- routes/index.js
+++ routes/index.js
@@ -1,43 +1,42 @@
 const express = require('express');
 const router = express.Router();
-const pg = require('pg');
+const { Pool } = require('pg');
+const QueryStream = require('pg-query-stream');
+const JSONStream = require('JSONStream');
 const path = require('path');
-const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/coleelam';
+// const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/coleelam';
+const connectionString = 'postgres://localhost:5432/coleelam';
 
-const client = new pg.Client(connectionString);
-
+// const client = new pg.Client(connectionString);
+const pool = new Pool({
+  connectionString: connectionString,
+});
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Eventem' });
 });
 
 router.post('/api/users', (req, res, next) => {
-  const results = [];
   // Grab data from http request
-  // console.log(req.body);
   const data = {username: req.body.username, email: req.body.email, pass_hash: req.body.pass_hash };
   // Get a Postgres client from the connection pool
-  client.connect(connectionString, (err, client, done) => {
-    // Handle connection errors
-    if(err) {
-      done();
-      console.log(err);
-      return res.status(500).json({success: false, data: err});
+  console.log(data);
+  console.log(connectionString);
+  pool.query('INSERT INTO users (username, email, pass_hash) values($1, $2, $3)',
+    [data.username, data.email, data.pass_hash], (err, res) => {
+    if (err) {
+      throw err
     }
-    // SQL Query > Insert Data
-    client.query('INSERT INTO users (username, email, pass_hash) values($1, $2, $3)',
-    [data.username, data.email, data.pass_hash]);
-    // SQL Query > Select Data
-    const query = client.query('SELECT * FROM users ORDER BY id ASC');
-    // Stream results back one row at a time
-    query.on('row', (row) => {
-      results.push(row);
-    });
-    // After all data is returned, close connection and return results
-    query.on('end', () => {
-      done();
-      return res.json(results);
-    });
+  });
+});
+
+router.get('/api/users', (req, res, next) => {
+  pool.query('SELECT user_id, username, email FROM users ORDER BY id ASC', (err, res) => {
+    if (err) {
+      throw err
+    }
+    console.log(res.rows);
+    return res.rows;
   });
 });
 
